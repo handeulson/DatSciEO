19ec6938796545ad3563b39f17bf141cff0d4602
handeulson <ge26zof@mytum.de>
2024-01-03 22:10:22 +0100

    Revert the last git push

--- utils/view_npy.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# Load the .npy file
-data = np.load(r'C:\DatSciEO-main\data\1123_delete_nan_samples_nanmean_B2\Acer_pseudoplatanus-0.npy')
-
-# Display the data
-print(data)
-print(data.shape)
